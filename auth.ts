2b4f9adb8de7fccd9195432a2a12fd16e287beb9
bGillooley <gillooley@gmail.com>
2024-06-01 15:31:39 +0100

    Fixed simple role based stuff

--- auth.ts
+++ auth.ts
@@ -7,17 +7,6 @@ const prisma = new PrismaClient();
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [
-    google({
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: `${profile.given_name} ${profile.family_name}`,
-          email: profile.email,
-          role: profile.role,
-        };
-      },
-    }),
-  ],
+  providers: [google],
   session: { strategy: "jwt" },
 });
